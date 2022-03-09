0588366a1f11df8b58670704aea07e512fafe108
afreefri <75045937+afreefri@users.noreply.github.com>
2022-03-08 23:08:38 -0500

    initial commit

---
+++ Animal In The Wild/Assets/GridS.cs
@@ -0,0 +1,254 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridS : MonoBehaviour
+{
+    public int size = 50;
+    public float scale = 0.1f; // scale for the perlin noise 
+    public float waterLevel = 0.4f; // any noise value over this number is not water and any noise value under this is water 
+    public Material terrainMaterial;
+    public Material edgeMaterial;
+
+    Cell[,] grid; 
+
+    void Start() 
+    {
+        //random offsets
+        //by adding random offsets to the noise map, we will generate a new map every time
+        float xOffSet = Random.Range(-10000f, 10000f);
+        float yOffSet = Random.Range(-10000f, 10000f);
+        
+        // create a noise map
+        //noise map will tell us what's water and what's not water
+        float[,] noiseMap = new float[size, size];
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                float noiseValue = Mathf.PerlinNoise(x * scale + xOffSet, y * scale + yOffSet);
+                noiseMap[x, y] = noiseValue;
+            }
+        }
+
+        //will create more of an island 
+        float[,] falloffMap = new float[size, size];
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                float xv = x / (float)size * 2 - 1;
+                float yv = y / (float)size * 2 - 1;
+                float v = Mathf.Max(Mathf.Abs(xv), Mathf.Abs(yv));
+                falloffMap[x, y] = Mathf.Pow(v, 3f) / (Mathf.Pow(v, 3f) + Mathf.Pow(2.2f - 2.2f * v, 3f));
+            }
+        }
+        
+
+        // initialize a grid with cells 
+        grid = new Cell[size, size];
+        for(int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                Cell cell = new Cell();
+                float noiseValue = noiseMap[x, y];
+                noiseValue -= falloffMap[x, y];
+                cell.isWater = noiseValue < waterLevel; //anything over 0.4f is not water, anything under 0.4 is water
+                grid[x, y] = cell; 
+            }
+        }
+
+        DrawTerrainMesh(grid);
+        DrawEdgeMesh(grid);
+        DrawTexture(grid);
+    }
+
+    void DrawTerrainMesh(Cell[,] grid) // will take our array of cells and make it into a mesh
+    {
+        Mesh mesh = new Mesh();
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        List<Vector2> uvs = new List<Vector2>();
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                Cell cell = grid[x, y];
+                if (!cell.isWater)
+                {
+                    Vector3 a = new Vector3(x - .5f, 0, y + .5f);
+                    Vector3 b = new Vector3(x + .5f, 0, y + .5f);
+                    Vector3 c = new Vector3(x - .5f, 0, y - .5f);
+                    Vector3 d = new Vector3(x + .5f, 0, y - .5f);
+                    Vector2 uvA = new Vector2(x / (float)size, y / (float)size);
+                    Vector2 uvB = new Vector2((x + 1) / (float)size, y / (float)size);
+                    Vector2 uvC = new Vector2(x / (float)size, (y + 1) / (float)size);
+                    Vector2 uvD = new Vector2((x + 1) / (float)size, (y + 1) / (float)size);
+                    Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                    Vector2[] uv = new Vector2[] { uvA, uvB, uvC, uvB, uvD, uvC };
+                    for (int k = 0; k < 6; k++)
+                    {
+                        vertices.Add(v[k]);
+                        triangles.Add(triangles.Count);
+                        uvs.Add(uv[k]);
+                    }
+                }
+            }
+        }
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.RecalculateNormals();
+
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
+
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+
+    }
+
+    void DrawEdgeMesh(Cell[,] grid)
+    {
+        Mesh mesh = new Mesh();
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                Cell cell = grid[x, y];
+                if (!cell.isWater)
+                {
+                    if (x > 0)
+                    {
+                        Cell left = grid[x - 1, y];
+                        if (left.isWater)
+                        {
+                            Vector3 a = new Vector3(x - .5f, 0, y + .5f);
+                            Vector3 b = new Vector3(x - .5f, 0, y - .5f);
+                            Vector3 c = new Vector3(x - .5f, -1, y + .5f);
+                            Vector3 d = new Vector3(x - .5f, -1, y - .5f);
+                            Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                            for (int k = 0; k < 6; k++)
+                            {
+                                vertices.Add(v[k]);
+                                triangles.Add(triangles.Count);
+                            }
+                        }
+                    }
+                    if (x < size - 1)
+                    {
+                        Cell right = grid[x + 1, y];
+                        if (right.isWater)
+                        {
+                            Vector3 a = new Vector3(x + .5f, 0, y - .5f);
+                            Vector3 b = new Vector3(x + .5f, 0, y + .5f);
+                            Vector3 c = new Vector3(x + .5f, -1, y - .5f);
+                            Vector3 d = new Vector3(x + .5f, -1, y + .5f);
+                            Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                            for (int k = 0; k < 6; k++)
+                            {
+                                vertices.Add(v[k]);
+                                triangles.Add(triangles.Count);
+                            }
+                        }
+                    }
+                    if (y > 0)
+                    {
+                        Cell down = grid[x, y - 1];
+                        if (down.isWater)
+                        {
+                            Vector3 a = new Vector3(x - .5f, 0, y - .5f);
+                            Vector3 b = new Vector3(x + .5f, 0, y - .5f);
+                            Vector3 c = new Vector3(x - .5f, -1, y - .5f);
+                            Vector3 d = new Vector3(x + .5f, -1, y - .5f);
+                            Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                            for (int k = 0; k < 6; k++)
+                            {
+                                vertices.Add(v[k]);
+                                triangles.Add(triangles.Count);
+                            }
+                        }
+                    }
+                    if (y < size - 1)
+                    {
+                        Cell up = grid[x, y + 1];
+                        if (up.isWater)
+                        {
+                            Vector3 a = new Vector3(x + .5f, 0, y + .5f);
+                            Vector3 b = new Vector3(x - .5f, 0, y + .5f);
+                            Vector3 c = new Vector3(x + .5f, -1, y + .5f);
+                            Vector3 d = new Vector3(x - .5f, -1, y + .5f);
+                            Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                            for (int k = 0; k < 6; k++)
+                            {
+                                vertices.Add(v[k]);
+                                triangles.Add(triangles.Count);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+
+        GameObject edgeObj = new GameObject("Edge");
+        edgeObj.transform.SetParent(transform);
+
+        MeshFilter meshFilter = edgeObj.AddComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
+
+        MeshRenderer meshRenderer = edgeObj.AddComponent<MeshRenderer>();
+        meshRenderer.material = edgeMaterial;
+    }
+
+    void DrawTexture(Cell[,] grid)
+    {
+        Texture2D texture = new Texture2D(size, size);
+        Color[] colorMap = new Color[size * size];
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                Cell cell = grid[x, y];
+                if (cell.isWater)
+                    colorMap[y * size + x] = Color.blue;
+                else
+                    colorMap[y * size + x] = Color.green;
+            }
+        }
+        texture.filterMode = FilterMode.Point;
+        texture.SetPixels(colorMap);
+        texture.Apply();
+
+        // we add texture to material and the material will go on mesh renderer
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        meshRenderer.material = terrainMaterial;
+        meshRenderer.material.mainTexture = texture;
+    }
+
+    void OnDrawGizmos() 
+    {
+        if (!Application.isPlaying) return;
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                Cell cell = grid[x, y];
+                if (cell.isWater)
+                {
+                    Gizmos.color = Color.blue;
+                }
+                else
+                {
+                    Gizmos.color = Color.green;
+                }
+                Vector3 pos = new Vector3(x, 0, y);
+                Gizmos.DrawCube(pos, new Vector2(1,1));
+            }
+        }
+    }
+}
